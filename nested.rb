b99d83651c282ec18aed095034cc6017e5c54854
Tiffani Weaver <tlove.weaver@gmail.com>
2019-04-16 14:11:31 +0000

    Done.

--- nested.rb
+++ nested.rb
@@ -16,6 +16,7 @@ def hopper
         }
      }
 
+programmer_hash[:grace_hopper]
 
 end
 
@@ -37,6 +38,8 @@ def alan_kay_is_known_for
           :languages => ["C"]
         }
      }
+
+programmer_hash[:alan_kay][:known_for]
 end
 
 def dennis_ritchies_language
@@ -55,6 +58,9 @@ def dennis_ritchies_language
           :languages => ["C"]
         }
      }
+programmer_hash[:dennis_ritchie][:languages] = "C"
+programmer_hash[:dennis_ritchie][:languages]
+
 end
 
 def adding_matz
@@ -80,6 +86,12 @@ def adding_matz
         }
      }
 
+programmer_hash [:yukihiro_matsumoto] = {
+   :known_for => "Ruby",
+    :languages => ["LISP, C"]
+ }
+
+programmer_hash
     
 end
 
@@ -99,9 +111,9 @@ def changing_alan
           :languages => ["C"]
         }
      }
-     #change what Alan Kay is :known_for to the value of the alans_new_info variable. 
-     alans_new_info = "GUI"
-     
+
+programmer_hash[:alan_kay][:known_for] = "GUI"
+programmer_hash     
      
 end
 
@@ -121,5 +133,7 @@ def adding_to_dennis
           :languages => ["C"]
         }
      }
+programmer_hash[:dennis_ritchie][:languages] = ["C", "Assembly"]
+programmer_hash
 
 end
